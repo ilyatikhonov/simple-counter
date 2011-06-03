34622423a53eef7f4cfdb8a88532ef148e053b54
piha <pipiha@gmail.com>
2011-06-03 17:35:10 +0400

    interface now supports all types

--- interface.js
+++ interface.js
@@ -7,46 +7,76 @@ require('datejs');
 
 http.createServer(function (req, res) {
 	var query = querystring.parse(url.parse(req.url).query);
-	var prefixes = [];
+	if (if404(!query.type || !query.id, res)) return;
+	var keys = [];
+	var id = query.id;
 	query.type = query.type.split(',');
 	if (query.type.indexOf('total') != -1) {
-		prefixes.push(conf.redis.keyPrefix.total);
+		keys.push(['total', conf.redis.keyPrefix.total + id]);
 	}
-
+	if (query.type.indexOf('total-uniq') != -1) {
+		keys.push(['total-uniq', conf.redis.keyPrefix.uniq + conf.redis.keyPrefix.total + id]);
+	}
 	if (query.type.indexOf('today') != -1) {
-		prefixes.push[conf.redis.keyPrefix.byDay + Date.today().toString('yyyy-MM-dd') + ':'];
+		keys.push(['today', conf.redis.keyPrefix.byDay + getToday() + id]);
 	}
-
+	if (query.type.indexOf('today-uniq') != -1) {
+		keys.push(['today-uniq', conf.redis.keyPrefix.uniq + conf.redis.keyPrefix.byDay + getToday() + id]);
+	}
 	if (query.type.indexOf('week') != -1) {
-		
+		keys.push(['week', getWeek(conf.redis.keyPrefix.byDay, id)]);
 	}
-	
-	if (!query.id || prefixes.length == 0) {
-		res.writeHead(404, {'Content-type': 'text/plain'});
-		res.end();
-		return;
+	if (query.type.indexOf('week-uniq') != -1) {
+		keys.push(['week-uniq', getWeek(conf.redis.keyPrefix.uniq + conf.redis.keyPrefix.byDay, id)]);
 	}
 
-	redis.mget(prefix + query.id, function(err, value) {
-		if (query.jsonp_callback) {
-			res.writeHead(200, {'Content-type': 'application/x-javascript'});
-			res.end(query.jsonp_callback + '(\'' + value + '\');');
+	if (if404(keys.length == 0, res)) return;
+
+	var multi = redis.multi();
+	keys.forEach(function(row) {
+		var name = row[0];
+		var prefix = row[1];
+		if (prefix instanceof Array) {
+			multi.mget.apply(multi, prefix);
 		} else {
-			res.writeHead(200, {'Content-type': 'text/plain'});
-			res.end(value);
+			multi.get(prefix);
 		}
 	});
 	
-	/*redis.keys('*', function(err, data) {
-		data.forEach(function(key, x) {
-			redis.get(key, function(err, value) {
-				res.write(key + '\t' + value.toString() + '\n');
-				if (x == data.length - 1) {
-					res.end();
-				}
+	multi.exec(function(err, values) {
+		var result = {};
+		if (values.length == keys.length) {
+			values.forEach(function(value, i) {
+				result[keys[i][0]] = value;
 			});
-		});
-	})*/
-	
-	
+		}
+		if (query.jsonp_callback) {
+			res.writeHead(200, {'Content-type': 'application/x-javascript'});
+			res.end(query.jsonp_callback + '(\'' + JSON.stringify(result) + '\');');
+		} else {
+			res.writeHead(200, {'Content-type': 'text/plain'});
+			res.end(JSON.stringify(result));
+		}
+		
+	});
 }).listen(conf.interface.port, conf.interface.host);
+
+function if404(expr, res) {
+	if (expr) {
+		res.writeHead(404, {'Content-type': 'text/plain'});
+		res.end('not found');
+	}
+	return expr;
+}
+
+function getToday() {
+	return Date.today().toString('yyyy-MM-dd') + ':';
+}
+
+function getWeek(prefix, postfix) {
+	var ret = [];
+	for (var i = 0; i > -7; i--) {
+		ret.push([prefix + Date.today().add(i).days().toString('yyyy-MM-dd') + ':' + postfix]);
+	}
+	return ret;
+}
